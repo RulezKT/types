4642f0033ac473a125b719c9fa09c2d98afbd6b2
RR <9381804@gmail.com>
2025-02-03 12:51:27 +0100

    added + and - to Position

--- types.go
+++ types.go
@@ -5,3 +5,21 @@ type Position struct {
 	Y float64
 	Z float64
 }
+
+func (p *Position) Minus(p2 *Position) Position {
+
+	return Position{
+		X: p.X - p2.X,
+		Y: p.Y - p2.Y,
+		Z: p.Z - p2.Z,
+	}
+}
+
+// Plus
+func (p *Position) Plus(p2 *Position) Position {
+	return Position{
+		X: p.X + p2.X,
+		Y: p.Y + p2.Y,
+		Z: p.Z + p2.Z,
+	}
+}
